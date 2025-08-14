61b3471343c6ee01d2923ce9f2cfc0bf6db255a9
djamadar01 <129887073+djamadar01@users.noreply.github.com>
2025-08-14 11:16:46 +0530

    Add files via upload

---
+++ Mini Projects/Hospital_manamgement/DBconnection.java
@@ -0,0 +1,16 @@
+package Hospital_management;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Anu@123";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
+
